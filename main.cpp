6e232a70c6118e4e311b879211d2ac7ee58d5b2a
Sergey Gorbunov <svgorbunov83@yandex.ru>
2021-04-23 11:12:22 +0300

    Added boost::program_options dependency.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "boost/program_options.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+namespace opt = boost::program_options;
+
+int main(int argc, char *argv[]) {
+    opt::options_description desc("All options");
+    desc.add_options()
+            ("help", "produce help message");
+    opt::variables_map vm;
+    opt::store(opt::parse_command_line(argc, argv, desc), vm);
+    opt::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+
     return 0;
 }
